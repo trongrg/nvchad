25c9ccdbaa186a82b336af5c23da256953a608e5
Trong Tran <trong.tran@sstechvn.com>
2022-11-27 20:19:58 +0700

    tmp

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -4,7 +4,7 @@ local M = {}
 
 M.options = {
   nvChad = {
-    update_url = "https://github.com/NvChad/NvChad",
+    update_url = "https://github.com/trongrg/NvChad",
     update_branch = "main",
   },
 }
