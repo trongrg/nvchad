db002092e4c2e0177ba4664da3242aa1ad7e387d
Trong Tran <trong.tran@sstechvn.com>
2023-06-06 17:41:15 +0700

    Commit custom folder

--- init.lua
+++ init.lua
@@ -1,34 +1,12 @@
-vim.defer_fn(function()
-  pcall(require, "impatient")
-end, 0)
+-- local autocmd = vim.api.nvim_create_autocmd
 
-require "core"
-require "core.options"
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
 
--- setup packer + plugins
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  print "Cloning packer .."
-  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- install plugins + compile their configs
-  vim.cmd "packadd packer.nvim"
-  require "plugins"
-  vim.cmd "PackerSync"
-
-  -- install binaries from mason.nvim & tsparsers
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "PackerComplete",
-    callback = function()
-      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
-      require("packer").loader "nvim-treesitter"
-    end,
-  })
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "FantasqueSansM Nerd Font:h18"
 end
-
-pcall(require, "custom")
-
-require("core.utils").load_mappings()
